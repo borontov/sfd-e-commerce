a7f9c412b1792558cf0c7da53e477c53bc1f3623
borontov <anborontov@gmail.com>
2025-02-14 22:33:13 +0500

    add is_paid order property

--- orders/models.py
+++ orders/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 from common.models import BaseModel
 from orders.business_logic.constants import OrderStatus
+from transactions.constants import TransactionStatus
 
 
 class Order(BaseModel):
     email = models.EmailField()
     products = models.ManyToManyField('products.Product')
     status = models.CharField(max_length=255, choices=OrderStatus.choices, default=OrderStatus.PAYMENT_WAITING)
+
+    @property
+    def is_paid(self) -> bool:
+        return self.transactions.filter(
+            status__in=(
+                TransactionStatus.COMPLETED, TransactionStatus.PENDING
+            ).exists()
+        )
