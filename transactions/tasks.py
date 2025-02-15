44ea5f46917d2a52b15d222cbb04dde63e993e0a
borontov <anborontov@gmail.com>
2025-02-15 09:24:31 +0500

    remove redundant is_paid check in transaction_processing_simulation task; add select_related and select_for_update; add docstring and comments; add fail simulation

--- transactions/tasks.py
+++ transactions/tasks.py
@@ -1,7 +1,8 @@
+import random
+
 from celery import shared_task
 from django.db.transaction import atomic
 
-from orders.business_logic.constants import OrderStatus
 from transactions.constants import TransactionStatus
 from transactions.models import Transaction
 import logging
@@ -10,10 +11,23 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def transaction_processing_simulation():
-    for transaction in Transaction.objects.filter(status=TransactionStatus.PENDING):
-        with atomic():
-            if transaction.order.is_paid():
-                continue
-            transaction.status = TransactionStatus.COMPLETED
+    """
+    Simulate a real transaction processing by third-party service.
+
+    bulk_update is not used here because it does not trigger pre_save and post_save signals.
+    Alternatively, signals can be removed, and bulk_update can be used for better performance.
+    """
+    with atomic():  # atomic is used to ensure that all transactions are committed or rolled back as a whole
+        transactions = (
+            Transaction.objects
+            .select_for_update()  # lock rows for update
+            .select_related('order')  # prefetch related order
+            .filter(status=TransactionStatus.PENDING)
+        )
+
+        for transaction in transactions:
+            transaction.status = random.choice(
+                (TransactionStatus.COMPLETED, TransactionStatus.FAILED)
+            )
             transaction.save()
-        logger.info(f"Transaction {transaction.id} status changed to {transaction.status}")
+            logger.info(f"Transaction {transaction.id} status changed to {transaction.status}")
